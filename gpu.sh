e3f1b220cfdff2573cdd07121c9d955a7b9a2802
mubie4170 <146758855+mubie4170@users.noreply.github.com>
2023-10-02 18:12:04 +0100

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -15,7 +15,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xf31d61695bbD8179d8D5BAD51b8cc3ec0CC93f72/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
